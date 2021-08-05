b9f4f0142c4cabf60d26ae8132b93c1a6f11c182
DrOxlotus <alex.metz1024@gmail.com>
2021-08-05 04:40:22 -0500

    Added a bunch of stuff.

--- GBAutoDeposit.lua
+++ GBAutoDeposit.lua
@@ -2,8 +2,50 @@ local addonName, addonTable = ...
 local e = CreateFrame("Frame")
 local gold, silver, copper
 
+local function ShowTooltip(self, text)
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	GameTooltip:SetText(text)
+	GameTooltip:Show()
+end
+
+local function HideTooltip(self)
+	if GameTooltip:GetOwner() == self then
+		GameTooltip:Hide()
+	end
+end
+
+SLASH_GBAutoDeposit1 = "/gbad"
+SlashCmdList["GBAutoDeposit"] = function(message, editbox)
+	if (GBAutoDepositFrame:IsVisible()) then
+		GBAutoDepositFrame:Hide()
+    else
+		GBAutoDepositFrame:Show()
+		GBAutoDepositGoldBox:SetScript("OnEnter", function(self)
+			ShowTooltip(self, "The value to keep on hand after deposits.\nPlease enter a value between |cffFFFFFF1|r and |cffFFFFFF9999|r.\nThis value applies to all characters on the account.\n\n"
+				.. "|cffFFFFFFCurrent|r: " .. GetCoinTextureString(GBAutoDepositAmount))
+		end)
+
+		GBAutoDepositGoldBox:SetScript("OnLeave", function(self)
+			HideTooltip(self)
+		end)
+
+		GBAutoDepositGoldBox:SetScript("OnEnterPressed", function(self)
+			GBAutoDepositAmount = (self:GetText()) * 10000
+			GBAutoDepositGoldBox:SetText("")
+			GBAutoDepositFrame:Hide()
+		end)
+	end
+end
+
+e:RegisterEvent("ADDON_LOADED")
 e:RegisterEvent("GUILDBANKFRAME_OPENED")
 e:SetScript("OnEvent", function(self, event, ...)
+	if (event == "ADDON_LOADED") then
+		if (GBAutoDepositAmount == nil) then
+			-- If the value hasn't been set, then default to 50 gold.
+			GBAutoDepositAmount = 500000
+		end
+	end
 	if (event == "GUILDBANKFRAME_OPENED") then
 		local money = GetMoney()
 		if (money > GBAutoDepositAmount) then
