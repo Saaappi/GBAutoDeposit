c24f18707ec985b632de8b0934d90b9e79372a1d
Saaappi <alex.metz1024@gmail.com>
2022-10-26 00:40:36 -0700

    Removed the frame.xml file and ported the options over to Ace3.

--- GBAutoDeposit.lua
+++ GBAutoDeposit.lua
@@ -2,6 +2,56 @@ local addonName, addonTable = ...
 local e = CreateFrame("Frame")
 local gold, silver, copper
 
+GBAutoDeposit = LibStub("AceAddon-3.0"):NewAddon("GBAutoDeposit", "AceConsole-3.0")
+
+local options = {
+	name = addonName,
+	handler = HelpMePlay,
+	type = "group",
+	args = {
+		enable = {
+			name = "Enable",
+			order = 0,
+			desc = "Toggle the addon's functionality on or off.",
+			type = "toggle",
+			get = function() return GBAutoDepositOptions.Enabled end,
+			set = function(_, val) GBAutoDepositOptions.Enabled = val end,
+		},
+		optionsHeader = {
+			name = "Options",
+			order = 1,
+			type = "header",
+		},
+		amount = {
+			name = "Amount",
+			order = 2,
+			usage = "Enter an amount of gold the addon should keep in your possession. The remainder will be deposited to the guild vault.",
+			type = "input",
+			get = function() return GBAutoDepositOptions.Amount end,
+			set = function(_, val) GBAutoDepositOptions.Amount = tonumber(val) end,
+			validate = function(self, val)
+				if tonumber(val) and string.len(val) <= 7 then
+					return true
+				end
+				return "Please enter a numeric value from 1 to 9,999,999 (commas should not be used)."
+			end,
+		},
+	},
+}
+
+function GBAutoDeposit:OnInitialize()
+	LibStub("AceConfig-3.0"):RegisterOptionsTable("GBAutoDeposit_Main", options)
+	self.mainOptions = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("GBAutoDeposit_Main", addonName)
+	self:RegisterChatCommand("gbad", "SlashCommandHandler")
+	
+	-- Default Options
+	if GBAutoDepositOptions == nil then
+		GBAutoDepositOptions = {}
+		GBAutoDepositOptions.Enabled = false
+		GBAutoDepositOptions.Amount = 500000
+	end
+end
+
 local function ShowTooltip(self, text)
 	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
 	GameTooltip:SetText(text)
@@ -14,7 +64,6 @@ local function HideTooltip(self)
 	end
 end
 
-SLASH_GBAutoDeposit1 = "/gbad"
 SlashCmdList["GBAutoDeposit"] = function(message, editbox)
 	if (GBAutoDepositFrame:IsVisible()) then
 		GBAutoDepositFrame:Hide()
@@ -63,20 +112,8 @@ SlashCmdList["GBAutoDeposit"] = function(message, editbox)
 	end
 end
 
-e:RegisterEvent("ADDON_LOADED")
 e:RegisterEvent("GUILDBANKFRAME_OPENED")
 e:SetScript("OnEvent", function(self, event, ...)
-	if event == "ADDON_LOADED" then
-		if GBAutoDepositOptions == nil then
-			GBAutoDepositOptions = {}
-		end
-		
-		if GBAutoDepositOptions.Amount == nil then
-			-- Set the amount to 0 if the
-			-- option is already nil.
-			GBAutoDepositOptions.Amount = 0
-		end
-	end
 	if event == "GUILDBANKFRAME_OPENED" then
 		local money = GetMoney()
 		if (money > GBAutoDepositOptions.Amount) then
