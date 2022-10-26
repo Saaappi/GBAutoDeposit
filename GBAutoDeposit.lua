d03441007fa05d8d03e7e0b5168a8396a6370b54
Saaappi <alex.metz1024@gmail.com>
2022-10-26 01:21:46 -0700

    Fixed the rest of the addon.

--- GBAutoDeposit.lua
+++ GBAutoDeposit.lua
@@ -28,9 +28,9 @@ local options = {
 			usage = "Enter an amount of gold the addon should keep in your possession. The remainder will be deposited to the guild vault.",
 			type = "input",
 			get = function() return GBAutoDepositOptions.Amount end,
-			set = function(_, val) GBAutoDepositOptions.Amount = tonumber(val) end,
+			set = function(_, val) GBAutoDepositOptions.Amount = tonumber(val*10000) end,
 			validate = function(self, val)
-				if tonumber(val) and string.len(val) <= 7 then
+				if tonumber(val) then
 					return true
 				end
 				return "Please enter a numeric value from 1 to 9,999,999 (commas should not be used)."
@@ -53,43 +53,45 @@ function GBAutoDeposit:OnInitialize()
 end
 
 function GBAutoDeposit:SlashCommandHandler(cmd)
-	local cmd, arg1, arg2 = string.split(" ", cmd)
 	if not cmd or cmd == "" then
 		Settings.OpenToCategory(addonName)
 	end
 end
 
-e:RegisterEvent("GUILDBANKFRAME_OPENED")
+e:RegisterEvent("PLAYER_INTERACTION_MANAGER_FRAME_SHOW")
 e:SetScript("OnEvent", function(self, event, ...)
-	if event == "GUILDBANKFRAME_OPENED" then
-		local money = GetMoney()
-		if (money > GBAutoDepositOptions.Amount) then
-			money = money-GBAutoDepositOptions.Amount
-			-- Let's get the length of the string and split it.
-			-- Copper is the last 2 digits. Silver is the next 2.
-			-- Gold is the rest.
-			local length = string.len(money)
-			gold = string.sub(money, 0, length-4)
-			silver = string.sub(money, length-3, length-2)
-			copper = string.sub(money, length-1, length-0)
-			-- Click the Deposit button in the
-			-- Guild Bank UI.
-			if GBAutoDepositOptions.State then
-				GuildBankFrame.DepositButton:Click("LeftButton")
-				if (StaticPopup1:IsVisible()) then
-					-- Let's make sure the popup appeared before we
-					-- try to fill in the fields. Let's also place it
-					-- on a timer in case Blizzard gets mad about
-					-- the hastiness of the automation.
-					C_Timer.After(0, function()
-						C_Timer.After(1, function()
-							StaticPopup1MoneyInputFrameGold:SetText(gold)
-							StaticPopup1MoneyInputFrameSilver:SetText(silver)
-							StaticPopup1MoneyInputFrameCopper:SetText(copper)
-							StaticPopup1Button1:Click()
-							print("Money deposited: " .. GetCoinTextureString(money))
+	if event == "PLAYER_INTERACTION_MANAGER_FRAME_SHOW" then
+		local type = ...
+		if type == 10 then
+			local money = GetMoney()
+			if (money > GBAutoDepositOptions.Amount) then
+				money = money-GBAutoDepositOptions.Amount
+				-- Let's get the length of the string and split it.
+				-- Copper is the last 2 digits. Silver is the next 2.
+				-- Gold is the rest.
+				local length = string.len(money)
+				gold = string.sub(money, 0, length-4)
+				silver = string.sub(money, length-3, length-2)
+				copper = string.sub(money, length-1, length-0)
+				-- Click the Deposit button in the
+				-- Guild Bank UI.
+				if GBAutoDepositOptions.Enabled then
+					GuildBankFrame.DepositButton:Click("LeftButton")
+					if (StaticPopup1:IsVisible()) then
+						-- Let's make sure the popup appeared before we
+						-- try to fill in the fields. Let's also place it
+						-- on a timer in case Blizzard gets mad about
+						-- the hastiness of the automation.
+						C_Timer.After(0, function()
+							C_Timer.After(1, function()
+								StaticPopup1MoneyInputFrameGold:SetText(gold)
+								StaticPopup1MoneyInputFrameSilver:SetText(silver)
+								StaticPopup1MoneyInputFrameCopper:SetText(copper)
+								StaticPopup1Button1:Click()
+								print("Money deposited: " .. GetCoinTextureString(money))
+							end)
 						end)
-					end)
+					end
 				end
 			end
 		end
